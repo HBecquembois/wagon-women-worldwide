ed262cb5a1ab14c386100c5022b2706a1a0d5a29
urie lesly <lesly75019@gmail.com>
2023-06-06 17:37:23 +0200

    gem intaller mais pas de seed et ni de migration impossoble de teste

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  include PgSearch::Model
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :projects
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :users, only: [:index, :show]
   get "/dashboard", to: "pages#dashboard"
+  get "/projects", to: "projects#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
