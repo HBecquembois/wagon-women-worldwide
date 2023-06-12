b6d5ef83890b120620ff7473061e9a56a3a0714e
eugene dami <eugene.dami@yahoo.fr>
2023-06-12 18:15:24 +0200

    defined dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,11 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @favorited = current_user.all_favorited
     @user = current_user
+    @user_mentors =  @user.all_favorited
+    @user_projects = Project.where(user_id: @user.id)
+    @user_chats = Chatroom.where(user_a_id: @user.id) || Chatroom.where(user_b_id: @user.id)
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
     resources :messages, only: :create
   end
 
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
