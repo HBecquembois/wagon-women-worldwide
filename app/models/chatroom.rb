d92b152445a2e7a1e108a481791deea6af3c4b83
Helene Becquembois <hbecquembois@gmail.com>
2023-06-13 15:41:28 +0200

    added style to chatrooms and messages

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,8 @@ class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   belongs_to :user_a, class_name: 'User', foreign_key: :user_a_id
   belongs_to :user_b, class_name: 'User', foreign_key: :user_b_id
+
+  def interlocutor(user)
+    user == user_a ? user_b : user_a
+  end
 end
