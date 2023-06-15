29ae27887846a25015de5f692ba9a347d144a50f
eugene dami <eugene.dami@yahoo.fr>
2023-06-15 16:43:35 +0200

    Modale dans view de dashboard et ligne en plus dans user controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,15 +10,6 @@ class PagesController < ApplicationController
     @user_mentors =  @user.all_favorited
     @user_projects = Project.where(user_id: @user.id)
     @user_chats = Chatroom.where(user_a_id: @user.id) || Chatroom.where(user_b_id: @user.id)
+    @user_real_chats = Chatroom.joins(:messages)
   end
 end
-
-# def index
-#   @projects = Project.all
-#   @users = User.all
-#   if params[:query].present?
-#     @projects = Project.search_by_competences_and_users(params[:query])
-#   else
-#     @projects = Project.all
-#   end
-# end
